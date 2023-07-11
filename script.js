dec21879cf0d6f4d93401d45489c931e496c8cc7
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-07-11 18:51:43 +0000

    Add dark and light mode

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+// Date Generator
 const date = new Date();
 let month = date.getMonth() + 1;
 let day = date.getDate();
 let year = date.getFullYear();
-console.log(year);
 
 let datePara = document.querySelector('.date')
 
 datePara.textContent = `${month}/${day}/${year}`;
+
+
+
+// Color Mode
+let colorModeBttn = document.querySelector('.color-mode');
+let html = document.querySelector('html');
+let colorSvgDiv = document.querySelector('.color-mode-svg');
+let sunSvg = document.querySelector('.sun-svg')
+let moonSvg = document.querySelector('.moon-svg')
+
+    //default to light mode on page load
+html.classList.add('light-mode');
+colorSvgDiv.removeChild(moonSvg);
+
+colorModeBttn.addEventListener('click', () => changeColor())
+
+function changeColor() {
+    if (html.classList == 'light-mode') {
+        html.classList.remove('light-mode');
+        html.classList.add('dark-mode');
+        colorSvgDiv.removeChild(sunSvg);
+        colorSvgDiv.appendChild(moonSvg);
+    } else {
+        html.classList.remove('dark-mode');
+        html.classList.add('light-mode');
+        colorSvgDiv.removeChild(moonSvg);
+        colorSvgDiv.appendChild(sunSvg);
+    }
+}
+
